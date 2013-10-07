0f2137b8cf2bc8a9e1633ab7a476673cee82bcff
JP Fielding <jp.fielding@gmail.com>
2013-10-07 22:57:53 +0000

    more wirelog madness

--- wirelog.go
+++ wirelog.go
@@ -12,11 +12,12 @@ import (
 type WireLogConn struct {
 	log io.WriteCloser
 	conn net.Conn
+	// TODO make use of channels to make this perform well
 }
 
 func (w *WireLogConn) Read(b []byte) (n int, err error) {
 	read, err := w.conn.Read(b)
-	w.log.Write(b[0:read])
+	w.Write(b[0:read])
 	return read, err
 }
 func (w *WireLogConn) Write(b []byte) (n int, err error) {
@@ -48,11 +49,11 @@ type Dialer func(network, addr string) (net.Conn, error)
 func WireLog(log io.WriteCloser) Dialer {
 	return func(network, addr string) (net.Conn, error) {
 		conn, err := net.Dial(network, addr)
-		conn = &WireLogConn{
+		wire := WireLogConn{
 			log: log,
 			conn: conn,
 		}
-		return conn, err
+		return &wire, err
 	}
 }
 
