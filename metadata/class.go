f21570d00b28765c1a0b8ec07a18169aa6c72856
C. Paul Bond <paul.bond@threewide.com>
2015-01-22 19:44:40 -0500

    We can now parse class metadata in both compact and xml formats.

--- metadata/class.go
+++ metadata/class.go
@@ -1,15 +1,14 @@
 package metadata
 
-import (
-	"time"
-)
+import "encoding/xml"
 
 type MClass struct {
-	Version  string
-	Date     time.Time
-	Resource string
-	Classes  []Class
+	Version  string `xml:"Version,attr"`
+	Date     string `xml:"Date,attr"`
+	Resource string `xml:"Resource,attr"`
+	Class    []Class
 }
+
 type Class struct {
 	ClassName        string
 	StandardName     string
@@ -19,9 +18,46 @@ type Class struct {
 	TableDate        string
 	UpdateVersion    string
 	UpdateDate       string
-	ClassTimeStamp   *time.Time
+	ClassTimeStamp   string
 	DeletedFlagField string
 	DeletedFlagValue string
-	HasKeyIndex      *bool
-	OffsetSupport    *bool
+	HasKeyIndex      string // Bool?
+	OffsetSupport    string // Bool?
+}
+
+func (m *MClass) InitFromXml(p *xml.Decoder, t xml.StartElement) error {
+	err := p.DecodeElement(m, &t)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *MClass) InitFromCompact(p *xml.Decoder, t xml.StartElement) error {
+	cd, err := CompactData{}.Parse(p, t, "	")
+	if err != nil {
+		return err
+	}
+	m.Version = cd.Attrs["Version"]
+	m.Date = cd.Attrs["Date"]
+	m.Resource = cd.Attrs["Resource"]
+	for _, r := range cd.Data {
+		res := Class{}
+		res.ClassName = r["ClassName"]
+		res.StandardName = r["StandardName"]
+		res.VisibleName = r["VisibleName"]
+		res.Description = r["Description"]
+		res.TableVersion = r["TableVersion"]
+		res.TableDate = r["TableDate"]
+		res.UpdateVersion = r["UpdateVersion"]
+		res.UpdateDate = r["UpdateDate"]
+		res.ClassTimeStamp = r["ClassTimeStamp"]
+		res.DeletedFlagField = r["DeletedFlagField"]
+		res.DeletedFlagValue = r["DeletedFlagValue"]
+		res.HasKeyIndex = r["HasKeyIndex"]
+		res.OffsetSupport = r["OffsetSupport"]
+
+		m.Class = append(m.Class, res)
+	}
+	return nil
 }
