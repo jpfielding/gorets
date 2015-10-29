9791879c79540d3a66e8406170e652d8c99a4374
JP Fielding <jp.fielding@gmail.com>
2015-10-29 15:31:49 +0000

    ua auth unit test fix

--- client/ua_auth_test.go
+++ client/ua_auth_test.go
@@ -7,9 +7,13 @@ import (
 )
 
 func TestUaAuth(t *testing.T) {
-	uauth := calculateUaAuthHeader("user-agent", "user-agent-pw", "request-id", "session-id", "rets-version")
-	testutils.Assert(t, "Digest 052c1af08431d3cc9717a37f9d6de169" == uauth, "bad auth")
+	uaauth := UserAgentAuthentication{
+		UserAgent:         "user-agent",
+		UserAgentPassword: "user-agent-pw",
+	}
+	header1 := uaauth.header("request-id", "session-id", "rets-version")
+	testutils.Assert(t, "Digest 052c1af08431d3cc9717a37f9d6de169" == header1, "bad auth")
 
-	uauth2 := calculateUaAuthHeader("user-agent", "user-agent-pw", "", "", "rets-version")
-	testutils.Assert(t, "Digest 73cc7ccfe417292b1155c5ccee7fbdab" == uauth2, "bad auth")
+	header2 := uaauth.header("", "", "rets-version")
+	testutils.Assert(t, "Digest 73cc7ccfe417292b1155c5ccee7fbdab" == header2, "bad auth")
 }
