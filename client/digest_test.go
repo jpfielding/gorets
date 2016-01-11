fb40792482f1eefc5484e15f59d8413987983ab2
JP Fielding <jp.fielding@gmail.com>
2016-01-11 14:51:24 +0000

    removing noisy testing

--- client/digest_test.go
+++ client/digest_test.go
@@ -6,8 +6,9 @@ package client
 
 import (
 	"crypto/md5"
-	testutils "github.com/jpfielding/gorets/testutils"
 	"testing"
+
+	testutils "github.com/jpfielding/gorets/testutils"
 )
 
 func TestParseChallenge(t *testing.T) {
@@ -99,8 +100,6 @@ func TestDigest(t *testing.T) {
 
 	expected := `Digest username="user", realm="users@mris.com", nonce="31333739363738363932323632201e00230a639db77779b354d601ee5d2e", uri="/platinum/login", response="5f8d366fb430e9b395a84dba52247a35", algorithm="MD5", opaque="6e6f742075736564"`
 
-	t.Logf("CHALLENGE: %s", wwwAuthenticate)
-
 	method, uri := "GET", "/platinum/login"
 
 	actual := createNoQopDigest().CreateDigestResponse(username, password, method, uri)
@@ -113,8 +112,6 @@ func TestDigestQopAuth(t *testing.T) {
 
 	wwwAuthenticate := `Digest realm="rets@ris.rets.interealty.com",nonce="cbbaac704d6762fa2e4c37bb84ffddb8",opaque="77974390-589f-4ec6-b67c-4a9fc096c03f",qop="auth"`
 	expected := `Digest username="user", realm="rets@ris.rets.interealty.com", nonce="cbbaac704d6762fa2e4c37bb84ffddb8", uri="/rets/login", response="3b2ce903001cf00a264c69674d691526", qop=auth, nc=00000001, cnonce="6ac2c2eee85f5c33", algorithm="MD5", opaque="77974390-589f-4ec6-b67c-4a9fc096c03f"`
-
-	t.Logf("CHALLENGE: %s", wwwAuthenticate)
 
 	method, uri := "GET", "/rets/login"
 	cnonce := "6ac2c2eee85f5c33"
