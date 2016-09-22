af20efb76dee6ce3fb621d4b24fc291401239434
JP Fielding <jp.fielding@gmail.com>
2016-09-22 15:44:34 +0000

    skipping trying to convert to xml via writer for now

--- rets/metadata_compact.go
+++ rets/metadata_compact.go
@@ -3,7 +3,6 @@ package rets
 import (
 	"context"
 	"encoding/xml"
-	"fmt"
 	"io"
 )
 
@@ -79,79 +78,4 @@ type CompactMSystem struct {
 type CompactSystem struct {
 	SystemID    string `xml:"SystemID,attr"`
 	Description string `xml:"SystemDescription,attr"`
-}
-
-// ToXML ...
-func (cm CompactMetadata) ToXML(out io.Writer) error {
-	// TODO do we need to include the xml.Header directly?
-	out.Write([]byte(xml.Header))
-	// os.Stdout
-	enc := xml.NewEncoder(out)
-	enc.Indent("  ", "    ")
-
-	retsStart, retsEnd := startend("RETS", map[string]string{
-		"ReplyCode": fmt.Sprintf("%d", cm.Response.Code),
-		"ReplyText": cm.Response.Text,
-	})
-	enc.EncodeToken(retsStart)
-	msysStart, msysEnd := startend("METADATA-SYSTEM", map[string]string{
-		"Date":    cm.MSystem.Date,
-		"Version": cm.MSystem.Version,
-	})
-	enc.EncodeToken(msysStart)
-	sysStart, sysEnd := startend("SYSTEM", map[string]string{
-		"SystemID":          cm.MSystem.System.SystemID,
-		"SystemDescription": cm.MSystem.System.Description,
-	})
-	enc.EncodeToken(sysStart)
-	commentsStart, commentsEnd := startend("COMMENTS", map[string]string{})
-	enc.EncodeToken(commentsStart)
-	enc.EncodeToken(commentsEnd)
-
-	// INNNER
-
-	enc.EncodeToken(sysEnd)
-	enc.EncodeToken(msysEnd)
-	enc.EncodeToken(retsEnd)
-	return enc.Flush()
-}
-
-func startend(name string, attrs map[string]string) (xml.StartElement, xml.EndElement) {
-	elem := xml.Name{Local: name}
-	var attr []xml.Attr
-	for k, v := range attrs {
-		attr = append(attr, xml.Attr{Name: xml.Name{Local: k}, Value: v})
-	}
-	return xml.StartElement{Name: elem, Attr: attr}, xml.EndElement{elem}
-}
-
-// MetadataLookup .. TODO figure out what to do with this
-var MetadataLookup = map[string]string{
-	"METADATA-SYSTEM":                     "SYSTEM",
-	"METADATA-FOREIGN_KEY":                "ForeignKey",
-	"METADATA-FILTER":                     "Filter",
-	"METADATA-FILTER_TYPE":                "FilterType",
-	"METADATA-RESOURCE":                   "Resource",
-	"METADATA-CLASS":                      "Class",
-	"METADATA-TABLE":                      "Field",
-	"METADATA-UPDATE":                     "Update",
-	"METADATA-UPDATE_TYPE":                "UpdateType",
-	"METADATA-COLUMN_GROUP_SET":           "ColumnGroupSet",
-	"METADATA-COLUMN_GROUP":               "ColumnGroup",
-	"METADATA-COLUMN_GROUP_CONTROL":       "ColumnGroupControl",
-	"METADATA-COLUMN_GROUP_TABLE":         "ColumnGroupTable",
-	"METADATA-COLUMN_GROUP_NORMALIZATION": "ColumnGroupNormalization",
-
-	"METADATA-OBJECT":                 "Object",
-	"METADATA-SEARCHHELP":             "SearchHelp",
-	"METADATA-EDITMASK":               "EditMask",
-	"METADATA-UPDATEHELP":             "UpdateHelp",
-	"METADATA-LOOKUP":                 "Lookup",
-	"METADATA-LOOKUP_TYPE":            "LookupType",
-	"METADATA-VALIDATION_LOOKUP":      "ValidationLookup",
-	"METADATA-VALIDATION_LOOKUP_TYPE": "ValidationLookupType",
-
-	"METADATA-VALIDATION_EXTERNAL":      "ValidationExternal",
-	"METADATA-VALIDATION_EXTERNAL_TYPE": "ValidationExternalType",
-	"METADATA-VALIDATION_EXPRESSION":    "ValidationExpression",
 }
