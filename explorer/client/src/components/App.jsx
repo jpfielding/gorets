c3849308bcf5b4bc41bd150600e8298a1aa92e11
awestbro <aaron.westbrook@move.com>
2016-10-19 14:25:01 -0400

    Change Login to Connections. Add MetadataService

--- explorer/client/src/components/App.jsx
+++ explorer/client/src/components/App.jsx
@@ -12,7 +12,8 @@ export default class App extends React.Component {
       <div className="helvetica">
         <nav className="pa3 bg-dark-blue">
           <Link to="/" title="Home" className="link fw2 moon-gray b f4 dib mr3">RETS Explorer</Link>
-          <Link to="/login" title="Login" className="link moon-gray f6 dib mr3">Login</Link>
+          <Link to="/connections" title="Connections" className="link moon-gray f6 dib mr3">Connections</Link>
+          <Link to="/explorer" title="Explorer" className="link moon-gray f6 dib mr3">Explorer</Link>
         </nav>
         {this.props.children}
       </div>
---
+++ explorer/client/src/components/containers/Connections.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Fieldset, Field, createValue } from 'react-forms';
+import LoginService from 'services/LoginService';
+import PasswordForm from 'components/utils/PasswordForm';
+
+export default class Connections extends React.Component {
+
+  constructor(props) {
+    super(props);
+    const formValue = createValue({
+      value: null,
+      onChange: this.onChange.bind(this),
+    });
+    this.state = { formValue };
+  }
+
+  onChange(formValue) {
+    this.setState({ formValue });
+  }
+
+  onSubmit = () => {
+    console.log('Submitting: ', this.state.formValue.value);
+    LoginService
+      .login(this.state.formValue.value)
+      .then(response => {
+        console.log(response);
+      }).catch((err) => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Connections</h1>
+        <div className="flex flex-row justify-center">
+          <div className="w-60">
+            <Fieldset formValue={this.state.formValue}>
+              <Field select="url" label="Login URL" />
+              <Field select="username" label="Username" />
+              <Field select="password" label="Password" Input={PasswordForm} />
+              <Field select="user-agent" label="User Agent" />
+              <Field select="user-agent-pw" label="User Agent Password" Input={PasswordForm} />
+              <Field select="rets-version" label="Protocol Version" />
+              <Field select="id" label="Custom RETs Name" />
+              <button onClick={this.onSubmit}>Submit</button>
+            </Fieldset>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
