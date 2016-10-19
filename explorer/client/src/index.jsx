c77f4ec2d873eb078120e7b3425fbf6a065dd1bf
awestbro <aaron.westbrook@move.com>
2016-10-19 09:47:31 -0400

    Split a couple pages out into components. Added react router

--- explorer/client/src/index.jsx
+++ explorer/client/src/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import ReactDOM from 'react-dom';
+import App from 'components/App';
+import Home from 'components/containers/Home';
+import Login from 'components/containers/Login';
+import NotFound from 'components/containers/NotFound';
+
 import 'styles/app.css';
 
-ReactDOM.render(
-  <div className="helvetica">
-    <h1 className="f1">gorets react</h1>
-    <div className="flex flex-row justify-around">
-      <div className="box tc">test</div>
-      <div className="box tc">test</div>
-      <div className="box tc">test</div>
-    </div>
-  </div>,
-  document.getElementById('app')
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="login" component={Login} />
+      <Route path="*" component={NotFound} />
+    </Route>
+  </Router>
+  ), document.getElementById('app')
 );
