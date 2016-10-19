89774d5f3d5e990c2fa142d364af1dc5f44242a7
awestbro <aaron.westbrook@move.com>
2016-10-19 16:16:51 -0400

    Added Explorer Class selection

--- explorer/client/src/services/MetadataService.js
+++ explorer/client/src/services/MetadataService.js
@@ -7,6 +7,7 @@ export default class MetadataService {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
+      body: JSON.stringify({ id: 'aaarnc' }),
     });
   }
 
