d670692bca1ba34bc23824d76d6af37e9fc115ce
awestbro <aaron.westbrook@move.com>
2016-10-21 09:27:29 -0400

    Cleanup Search component. Object metadata won't show if keyField is not in searchResults

--- explorer/client/src/components/containers/Search.jsx
+++ explorer/client/src/components/containers/Search.jsx
@@ -71,7 +71,6 @@ class Search extends React.Component {
           this.setState({ metadata: Search.emptyMetadata });
           return;
         }
-        console.log(json.result.Metadata);
         this.setState({
           metadata: json.result.Metadata,
         });
@@ -86,10 +85,14 @@ class Search extends React.Component {
         selectedResource = resource;
       }
     });
-    if (!selectedResource) {
+    const { searchResults } = this.state;
+    if (!selectedResource || !searchResults.result) {
       return null;
     }
     const keyField = selectedResource.KeyField;
+    if (!searchResults.result.columns.includes(keyField)) {
+      return null;
+    }
     const metadataObjects = selectedResource['METADATA-OBJECT']['Object'];
     if (metadataObjects.length === 0) {
       return null;
@@ -101,6 +104,7 @@ class Search extends React.Component {
     const rowGetter = (i) => metadataObjects[i];
     return (
       <div>
+        <div className="b mv2">Object Metadata Types</div>
         <ReactDataGrid
           columns={columns}
           rowGetter={rowGetter}
@@ -157,7 +161,6 @@ class Search extends React.Component {
           <div>
             <div className="b mb2">Search Results</div>
             {this.renderSearchResultsTable()}
-            <div className="b mv2">Object Metadata Types</div>
             {this.renderObjectMetadata()}
           </div>
           {/* <div>Search parameters:
