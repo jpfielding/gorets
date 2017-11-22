cec30f4a55684e37d42d8b2f9d49f2e11b2eea72
JP Fielding <jp.fielding@gmail.com>
2017-11-22 15:57:54 +0000

    added age limit on metadata before sending re-request

---
+++ explorer/util_test.go
@@ -0,0 +1,19 @@
+package explorer_test
+
+import "testing"
+
+func TestLoad(t *testing.T) {
+	// TODO this is here to remind me that im a bad person
+}
+
+func TestStore(t *testing.T) {
+	// TODO this is here to remind me that im a bad person
+}
+
+func TestExists(t *testing.T) {
+	// TODO this is here to remind me that im a bad person
+}
+
+func TestExistsSince(t *testing.T) {
+	// TODO this is here to remind me that im a bad person
+}
