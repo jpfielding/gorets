c22f3a6575e21e1d157b86fa25026c9ed82538ec
JP Fielding <jp.fielding@gmail.com>
2018-08-10 16:40:04 +0000

    realiginging packages

--- web/explorer/client/webpack.config.js
+++ web/explorer/client/webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     extensions: ['', '.js', '.jsx'],
   },
   output: {
-    path: path.join(__dirname, '/../../bin/explorer'),
+    path: path.join(__dirname, '/../../../bin/explorer'),
     filename: 'bundle.js',
     publicPath: `${config.staticAssetPath}/`,
   },
